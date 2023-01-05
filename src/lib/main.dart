9f61529374874ce31400d1b531a214dbc1e2beb8
M4teusL1ma <mateuslima546@outlook.com>
2023-01-04 21:50:20 -0300

    Feature: Modal with a form and creation of cards

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,19 +1,100 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const ExpensesApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          appBar: AppBar(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Tênis de corrida',
+        value: 307.76,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 200.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: title,
+        value: value,
+        date: DateTime.now());
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  void _openTransactionFormModal(BuildContext context) async {
+    showModalBottomSheet(
+        context: context,
+        builder: ((_) {
+          return TransactionForm(
+            onSubmit: _addTransaction,
+          );
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
         title: const Text("Despesas Pessoais"),
         backgroundColor: Colors.purple,
-      )),
+        actions: [
+          IconButton(
+            onPressed: () => _openTransactionFormModal(context),
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Text("Gráfico"),
+              ),
+            ),
+            TransactionList(transactions: _transactions),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openTransactionFormModal(context),
+        backgroundColor: Colors.amber,
+        child: const Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
